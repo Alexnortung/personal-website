cc28d9596c93295c1ae00b37f6e8b909744745be
Alexnortung <alex_nortung@live.dk>
2017-09-28 17:59:52 +0200

    added imag and commented navigation out

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 var myCanvas;
 var points = [];
 var maxPoints = 5;
+var backgroundColor = [242, 237, 230];
+var warmColor = [229, 155, 103];
+var objects = [];
 
 
 function fullCanvas() {
@@ -18,19 +21,26 @@ function setup() {
 
 	//myCanvas.canvas.classList.add("zm1");
 
+	
+
 }
 
 function draw() {
-	background(242, 237, 230);
+	background(backgroundColor[0], backgroundColor[1], backgroundColor[2]);
 	if (mouseX != 0 || mouseY != 0) {
 		points.unshift({x:mouseX,y:mouseY});
 	}
 	if (points.length > maxPoints) {
+		stroke(0);
 		for (var i = 0; i < maxPoints; i++) {
+			
 			line(points[i].x,points[i].y,points[i+1].x,points[i+1].y);
 		}
 		
 		points.splice(maxPoints, 1);
+	}
+	for (var i = 0; i < objects.length; i++) {
+		//objects[i].draw();
 	}
 	
 }
@@ -39,3 +49,6 @@ function draw() {
 window.onresize = function(){
 	fullCanvas();
 }
+
+
+
