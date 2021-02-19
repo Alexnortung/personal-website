1d1064a35b385c5d67472a6414b59ac3b0fed17f
Alexander Nortung <alex_nortung@live.dk>
2021-02-19 13:04:13 +0100

    debug deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,10 +6,12 @@ set -xe
  # setup ssh agent, git config and remote
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/travis_rsa
+echo "added ssh-key successfully"
 
 git remote add deploy "travis@nortung.dk:/var/www/nortung.dk"
 git config user.name "Travis CI"
 git config user.email "travis@nortung.dk"
+echo "git config updated successfully"
 
 # commit compressed files and push it to remote
 rm -f .gitignore
